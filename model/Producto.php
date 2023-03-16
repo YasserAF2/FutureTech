070a6064f23f14ae67004c0f98db7d5a1c61e83a
YasserAF2 <9087@cifpceuta.es>
2023-03-16 15:59:44 +0100

    producto

--- model/Producto.php
+++ model/Producto.php
@@ -8,4 +8,44 @@ class Producto
     private $precio;
     private $imagen;
     private $cantidad;
+
+    public function __construct($id_producto, $nombre, $descripcion, $precio, $imagen, $cantidad)
+    {
+        $this->id_producto = $id_producto;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+        $this->imagen = $imagen;
+        $this->cantidad = $cantidad;
+    }
+
+    public function getIdProducto()
+    {
+        return $this->id_producto;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
 }
