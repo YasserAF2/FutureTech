f4f416d8eb0ede7bf544c4e8e066f68b956ace74
YasserAF2 <9087@cifpceuta.es>
2023-05-09 10:40:33 +0200

    cantidad carrito arreglada

--- view/carrito.php
+++ view/carrito.php
@@ -2,6 +2,21 @@
 $tienda = new Tienda();
 $productos = $dataToView['productos'];
 $precio_total = $dataToView['precio_total'];
+
+// Agrupar los productos por id_producto y llevar un registro de la cantidad
+$productos_agrupados = [];
+foreach ($productos as $producto) {
+    $id_producto = $producto['id_producto'];
+    if (isset($productos_agrupados[$id_producto])) {
+        $productos_agrupados[$id_producto]['cantidad'] += $producto['cantidad'];
+    } else {
+        $productos_agrupados[$id_producto] = [
+            'nombre' => $tienda->obtenerNombreProducto($id_producto),
+            'precio' => $producto['precio'],
+            'cantidad' => $producto['cantidad']
+        ];
+    }
+}
 ?>
 <div>
     <h1> CARRITO DE COMPRA</h1>
@@ -14,23 +29,20 @@ $precio_total = $dataToView['precio_total'];
                     <th>Producto</th>
                     <th>Precio</th>
                     <th>Cantidad</th>
-                    <!--                 
-                        <th>Subtotal</th>
-                    -->
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($productos as $producto) : ?>
+                <?php foreach ($productos_agrupados as $producto) : ?>
                     <tr>
-                        <td><?php echo $tienda->obtenerNombreProducto($producto['id_producto']); ?></td>
+                        <td><?php echo $producto['nombre']; ?></td>
                         <td><?php echo $producto['precio']; ?></td>
                         <td><?php echo $producto['cantidad']; ?></td>
+                        <td><i class="fas fa-trash-alt"></i></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
         <p class="precio-total">Precio total: <?php echo $precio_total; ?> euros.</p>
     <?php endif; ?>
-
-
 </div>
